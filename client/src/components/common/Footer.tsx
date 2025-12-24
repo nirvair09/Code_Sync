07f9e1ab919ec3cf30ae44f8703745e3fae1dcb6
nirvair09 <chandradeep262003@gmail.com>
2025-12-24 11:04:27 +0530

    feat: add GitHub corner and footer components, and remove all content from README.md

--- client/src/components/common/Footer.tsx
+++ client/src/components/common/Footer.tsx
@@ -4,10 +4,10 @@ function Footer() {
             <span>
                 Build with ❤️ by{" "}
                 <a
-                    href="https://github.com/sahilatahar"
+                    href="https://github.com/nirvair09"
                     className="text-primary underline underline-offset-1"
                 >
-                    sahilatahar
+                    nirvair09
                 </a>
             </span>
         </footer>
